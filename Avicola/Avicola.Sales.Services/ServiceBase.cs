9d897a4cfe5f03bb7650c3e054cf3bf3b35cbfa4
tromps <jrossiq@gmail.com>
2015-08-22 09:49:27 -0300

    Services

---
+++ Avicola/Avicola.Sales.Services/ServiceBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Avicola.Sales.Data.Interfaces;
+using Framework.Data.Repository;
+using Avicola.Services.Common;
+
+namespace Avicola.Sales.Services
+{
+    public class ServiceBase : IServive
+    {
+        protected ISalesUow Uow { get; set; }
+
+        protected IUowFactory UowFactory { get; set; }
+
+        #region IDisposable
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (Uow != null)
+                {
+                    Uow.Dispose();
+                    Uow = null;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
