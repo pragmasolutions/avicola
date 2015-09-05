d96407577054aeb4ba16aeb31c13f8c4226986a9
tromps <jrossiq@gmail.com>
2015-09-05 11:26:03 -0300

    create standartds

---
+++ Avicola/Avicola.Web/Scripts/app/office-create-standard.js
@@ -0,0 +1,94 @@
+﻿var standardForm = function () {
+    var _showSecondValue,
+        _standardName,
+        _geneticLineName,
+        _measureUnity,
+
+    generateChart = function() {
+        var categories = [];
+        var series;
+
+        if (_showSecondValue) {
+            var firstSerie = {
+                name: 'Mínimo',
+                data: []
+            }
+            $('.item-value.first-value .standard-item-value').each(function (i, item) {
+                categories.push(i + 1);
+                firstSerie.data.push(parseFloat($(item).val()));
+            });
+            var secondSerie = {
+                name: 'Máximo',
+                data: []
+            }
+            $('.item-value.second-value .standard-item-value').each(function (i, item) {
+                secondSerie.data.push(parseFloat($(item).val()));
+            });
+            series = [firstSerie, secondSerie];
+        } else {
+            firstSerie = {
+                name: _standardName,
+                data: []
+            }
+            $('.item-value.first-value .standard-item-value').each(function (i, item) {
+
+                categories.push(i + 1);
+                firstSerie.data.push(parseFloat($(item).val()));
+            });
+            series = [firstSerie];
+        }
+
+        $('#chart-container').highcharts({
+            title: {
+                text: _geneticLineName,
+                x: -20 //center
+            },
+            subtitle: {
+                text: 'Standard: ' + _standardName,
+                x: -20
+            },
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                title: {
+                    text: _standardName + ' (' + _measureUnity + ')'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }]
+            },
+            tooltip: {
+                valueSuffix: ' ' + _measureUnity
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle',
+                borderWidth: 0
+            },
+            series: series
+        });
+    },
+    initialize = function (options) {
+        _showSecondValue = options.showSecondValue;
+        _standardName = options.standardName;
+        _geneticLineName = options.geneticLineName;
+        _measureUnity = options.measureUnity;
+
+        $('.standard-item-value').change(function () {
+            if (!$(this).val()) {
+                $(this).val('0,00');
+            }
+            generateChart();
+        });
+
+        generateChart();
+    }
+
+    return {
+        initialize: initialize
+    };
+}();
