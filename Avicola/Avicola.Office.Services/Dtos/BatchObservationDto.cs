47cc7d6b81594f857ef669a3d10863fe41ab5ada
nicolasdogi <nicolasdogi@gmail.com>
2015-09-17 13:48:21 -0300

    ObservationDto

---
+++ Avicola/Avicola.Office.Services/Dtos/BatchObservationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Avicola.Office.Entities;
+using Framework.Common.Mapping;
+
+namespace Avicola.Office.Services.Dtos
+{
+    public class BatchObservationDto : IMapFrom<BatchObservation>
+    {
+        public System.Guid Id { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public System.DateTime ObservationDate { get; set; }
+        public int Week { get; set; }
+        public int Day { get; set; }        
+        public string Content { get; set; }              
+    }
+}
