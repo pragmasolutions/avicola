68b52d6c3f83fae332a8e8400864d15e3bcc58d8
tromps <jrossiq@gmail.com>
2015-09-17 21:46:34 -0300

    Fusion con form principal

--- Avicola/Avicola.Production.Win/Forms/Batchs/FrmBatchManager.cs
+++ Avicola/Avicola.Production.Win/Forms/Batchs/FrmBatchManager.cs
@@ -7,6 +7,7 @@ using Avicola.Office.Services.Dtos;
 using Avicola.Office.Services.Interfaces;
 using Avicola.Production.Win.Infrastructure;
 using Avicola.Production.Win.Properties;
+using Telerik.WinControls;
 using Telerik.WinControls.UI;
 
 namespace Avicola.Production.Win.Forms.Batchs
@@ -14,13 +15,14 @@ namespace Avicola.Production.Win.Forms.Batchs
     public partial class FrmBatchManager : FrmBase
     {
         private readonly IStateController _stateController;
+        private readonly IServiceFactory _serviceFactory;
 
-        public FrmBatchManager(IFormFactory formFactory, IStateController stateController)
+        public FrmBatchManager(IFormFactory formFactory, IStateController stateController, IServiceFactory serviceFactory)
         {
             FormFactory = formFactory;
 
             _stateController = stateController;
-
+            _serviceFactory = serviceFactory;
             InitializeComponent();
         }
 
@@ -45,6 +47,44 @@ namespace Avicola.Production.Win.Forms.Batchs
         private void btnEstandares_Click(object sender, EventArgs e)
         {
             TransitionManager.LoadStandardSelectionView();
+        }
+
+        private void btnEndBatch_Click(object sender, EventArgs e)
+        {
+            OpenEndBatchForm();
+        }
+        
+        private void OpenEndBatchForm()
+        {
+            var form = Application.OpenForms.OfType<FrmEndBatch>().FirstOrDefault();
+            if (form != null)
+            {
+                form.Activate();
+            }
+            else
+            {
+                var frm = FormFactory.Create<FrmEndBatch>(_stateController.CurrentSelectedBatch.Id);
+                frm.BatchEnded += FrmOnBatchEnded;
+                frm.Show();
+            }
+        }
+
+        private void FrmOnBatchEnded(object sender, Batch batch)
+        {
+            TransitionManager.LoadBatchSelectionView();
+        }
+
+        private void btnEliminarLote_Click(object sender, EventArgs e)
+        {
+            DialogResult ds = RadMessageBox.Show(this, "Si elimina el lote se perderán todos los datos cargados asociados al mismo. \n\nEstá seguro que desea continuar?", "Confirmación", MessageBoxButtons.YesNo, RadMessageIcon.Question);
+            if (ds.ToString() == "Yes")
+            {
+                using (var batchService = _serviceFactory.Create<IBatchService>())
+                {
+                    batchService.Delete(_stateController.CurrentSelectedBatch.Id);
+                    TransitionManager.LoadBatchSelectionView();
+                }
+            }
         }
     }
 }
