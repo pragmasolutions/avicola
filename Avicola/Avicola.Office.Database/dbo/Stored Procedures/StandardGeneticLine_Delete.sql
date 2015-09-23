02927c0e562d4198359a4e26b456a2fcefc87e8b
tromps <jrossiq@gmail.com>
2015-09-22 22:51:30 -0300

    standardGeneticLine_delete

---
+++ Avicola/Avicola.Office.Database/dbo/Stored Procedures/StandardGeneticLine_Delete.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE StandardGeneticLine_Delete
+	@Id uniqueidentifier
+AS
+BEGIN
+	UPDATE M
+	SET IsDeleted = 1 
+	FROM Measure M
+		INNER JOIN StandardItem SI
+			ON M.StandardItemId = SI.Id
+	WHERE SI.StandardGeneticLineId = @Id
+
+	UPDATE SI
+	SET IsDeleted = 1 
+	FROM Measure M
+		INNER JOIN StandardItem SI
+			ON M.StandardItemId = SI.Id
+	WHERE SI.StandardGeneticLineId = @Id
+
+	UPDATE StandardGeneticLine
+	SET IsDeleted = 1
+	WHERE Id = @Id
+END
+GO
