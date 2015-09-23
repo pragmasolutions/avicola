217d552fb9f6c18abd3d29eb2ab4fa7f05862266
nicolasdogi <nicolasdogi@gmail.com>
2015-09-22 21:46:43 -0300

    ABM Batch Vaccine

---
+++ Avicola/Avicola.Office.Services/Dtos/BatchVaccineDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avicola.Office.Services.Dtos
+{
+    public class BatchVaccineDto
+    {
+        public Guid Id { get; set; }
+        public Guid BatchId { get; set; }
+        public Guid VaccineId { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? RecommendedDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsDeleted { get; set; }
+        public string VaccineName { get; set; }
+    }
+}
