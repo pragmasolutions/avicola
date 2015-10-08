e7c7bd65b137bcccd56b9a7eb1853a0195abfb81
federicofrank <defe.frank@gmail.com>
2015-10-08 14:29:58 -0300

    Agregar logo a la Win de produccion

--- Avicola/Avicola.Production.Win/Forms/FrmBase.Designer.cs
+++ Avicola/Avicola.Production.Win/Forms/FrmBase.Designer.cs
@@ -37,12 +37,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(292, 270);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FrmBase";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "FrmBase";
+            this.ThemeName = "TelerikMetroBlue";
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
 
