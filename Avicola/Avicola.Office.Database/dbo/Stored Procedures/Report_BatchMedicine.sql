8c2eb39d2e0f3894a0722d002ecbcdf7eb92c98c
nicolasdogi <nicolasdogi@gmail.com>
2015-10-10 15:17:48 -0300

    Create Medicine tabla, BatchMedicine table, Report_BatchMEdicine

---
+++ Avicola/Avicola.Office.Database/dbo/Stored Procedures/Report_BatchMedicine.sql
@@ -0,0 +1,26 @@
+﻿CREATE PROCEDURE [dbo].[Report_BatchMedicine]
+	@BatchId UNIQUEIDENTIFIER,
+	@StageId UNIQUEIDENTIFIER
+AS
+BEGIN
+	
+	DECLARE @StageName VARCHAR(50)
+	SET @StageName = (SELECT TOP 1 LOWER(Name) FROM Stage WHERE Id = @StageId)
+	
+	SELECT 
+		B.Number,
+		BM.CreatedDate,
+		BM.StartDate,
+		BM.EndDate,
+		Medicine = M.Name,
+		Observation = M.Observation
+	FROM BatchMedicine BM INNER JOIN Batch B
+		ON BM.BatchId = B.Id INNER JOIN GeneticLine GL 
+		ON B.GeneticLineId = GL.Id INNER JOIN Medicine M
+		ON BM.MedicineId = M.Id
+	WHERE B.Id = BatchId 
+		AND ((@StageName = 'cría y precría' AND BM.CreatedDate < DATEADD(DAY, (GL.WeeksInBreeding * 7), B.DateOfBirth))
+		OR (@StageName = 'postura' AND BM.CreatedDate > DATEADD(DAY, (GL.WeeksInBreeding * 7), B.DateOfBirth)))
+	ORDER BY BM.CreatedDate 
+END
+GO
---
+++ Avicola/Avicola.Office.Database/dbo/Tables/BatchMedicine.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[BatchMedicine](
+	[Id] [uniqueidentifier] NOT NULL,
+	[BatchId] [uniqueidentifier] NOT NULL,
+	[MedicineId] [uniqueidentifier] NOT NULL,
+	[CreatedDate] [datetime2](7) NOT NULL,
+	[StartDate] [datetime2](7) NOT NULL,
+	[EndDate] [datetime2](7) NULL,
+	[IsDeleted] BIT NOT NULL DEFAULT 0, 
+ CONSTRAINT [PK_BatchMedicine] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY], 
+    CONSTRAINT [FK_BatchMedicine_Batch] FOREIGN KEY ([BatchId]) REFERENCES [Batch]([Id]), 
+    CONSTRAINT [FK_BatchMedicine_Medicine] FOREIGN KEY ([MedicineId]) REFERENCES [Medicine]([Id])
+) ON [PRIMARY]
