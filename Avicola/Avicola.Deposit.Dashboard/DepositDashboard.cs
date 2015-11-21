23a90985eeb5db4b8b2113034113efc83d96cf1f
tromps <jrossiq@gmail.com>
2015-11-21 14:12:52 -0300

    Avicola.Deposit.Dashboard

---
+++ Avicola/Avicola.Deposit.Dashboard/DepositDashboard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Avicola.Deposit.Dashboard
+{
+    public partial class DepositDashboard : Form
+    {
+        public DepositDashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
