652bf23c902db50f9d34c4d05d9ab4adba1d0720
tromps <jrossiq@gmail.com>
2015-11-21 16:57:49 -0300

    cosas muy crazy

--- Avicola/Avicola.Deposit.Dashboard/DepositDashboard.Designer.cs
+++ Avicola/Avicola.Deposit.Dashboard/DepositDashboard.Designer.cs
@@ -29,10 +29,17 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DepositDashboard));
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn6 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn7 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn8 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn9 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn10 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.TableViewDefinition tableViewDefinition2 = new Telerik.WinControls.UI.TableViewDefinition();
             Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn1 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
             Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn2 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
             Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn3 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
             Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn4 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
+            Telerik.WinControls.UI.GridViewTextBoxColumn gridViewTextBoxColumn5 = new Telerik.WinControls.UI.GridViewTextBoxColumn();
             Telerik.WinControls.UI.TableViewDefinition tableViewDefinition1 = new Telerik.WinControls.UI.TableViewDefinition();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
@@ -50,6 +57,10 @@
             this.panel5 = new System.Windows.Forms.Panel();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
             this.label5 = new System.Windows.Forms.Label();
+            this.dgvPreparedOrders = new Telerik.WinControls.UI.RadGridView();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label6 = new System.Windows.Forms.Label();
             this.dgvPendingOrders = new Telerik.WinControls.UI.RadGridView();
             this.tableLayoutPanel1.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
@@ -62,6 +73,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel5.SuspendLayout();
             this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreparedOrders)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreparedOrders.MasterTemplate)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders.MasterTemplate)).BeginInit();
             this.SuspendLayout();
@@ -69,15 +84,16 @@
             // tableLayoutPanel1
             // 
             this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 585F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 700F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel6, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1095, 603);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1348, 603);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // tableLayoutPanel2
@@ -92,7 +108,7 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 300F));
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(579, 597);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(694, 597);
             this.tableLayoutPanel2.TabIndex = 0;
             // 
             // panel1
@@ -103,10 +119,10 @@
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Location = new System.Drawing.Point(10, 10);
             this.panel1.Margin = new System.Windows.Forms.Padding(10);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(566, 296);
+            this.panel1.Size = new System.Drawing.Size(674, 280);
             this.panel1.TabIndex = 0;
             // 
             // panel4
@@ -116,7 +132,7 @@
             this.panel4.Controls.Add(this.pictureBox3);
             this.panel4.Location = new System.Drawing.Point(14, 196);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(515, 62);
+            this.panel4.Size = new System.Drawing.Size(648, 62);
             this.panel4.TabIndex = 3;
             // 
             // label4
@@ -146,7 +162,7 @@
             this.panel3.Controls.Add(this.pictureBox2);
             this.panel3.Location = new System.Drawing.Point(14, 128);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(515, 62);
+            this.panel3.Size = new System.Drawing.Size(648, 62);
             this.panel3.TabIndex = 2;
             // 
             // label3
@@ -176,7 +192,7 @@
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Location = new System.Drawing.Point(14, 61);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(515, 62);
+            this.panel2.Size = new System.Drawing.Size(648, 62);
             this.panel2.TabIndex = 1;
             // 
             // label2
@@ -217,7 +233,7 @@
             this.panel5.Location = new System.Drawing.Point(10, 310);
             this.panel5.Margin = new System.Windows.Forms.Padding(10);
             this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(552, 277);
+            this.panel5.Size = new System.Drawing.Size(674, 277);
             this.panel5.TabIndex = 1;
             // 
             // tableLayoutPanel3
@@ -225,14 +241,14 @@
             this.tableLayoutPanel3.ColumnCount = 1;
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel3.Controls.Add(this.label5, 0, 0);
-            this.tableLayoutPanel3.Controls.Add(this.dgvPendingOrders, 0, 1);
-            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel3.Controls.Add(this.dgvPreparedOrders, 0, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel3.Size = new System.Drawing.Size(550, 259);
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(672, 275);
             this.tableLayoutPanel3.TabIndex = 0;
             // 
             // label5
@@ -240,16 +256,103 @@
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(102, 14);
+            this.label5.Location = new System.Drawing.Point(183, 14);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(345, 32);
+            this.label5.Size = new System.Drawing.Size(305, 32);
             this.label5.TabIndex = 1;
-            this.label5.Text = "PEDIDOS PENDIENTES";
+            this.label5.Text = "PEDIDOS ARMADOS";
+            // 
+            // dgvPreparedOrders
+            // 
+            this.dgvPreparedOrders.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvPreparedOrders.Location = new System.Drawing.Point(10, 70);
+            this.dgvPreparedOrders.Margin = new System.Windows.Forms.Padding(10);
+            // 
+            // 
+            // 
+            gridViewTextBoxColumn6.FieldName = "OrderId";
+            gridViewTextBoxColumn6.HeaderText = "OrderId";
+            gridViewTextBoxColumn6.IsVisible = false;
+            gridViewTextBoxColumn6.Name = "OrderId";
+            gridViewTextBoxColumn7.FieldName = "Client";
+            gridViewTextBoxColumn7.HeaderText = "Cliente";
+            gridViewTextBoxColumn7.MaxWidth = 5000;
+            gridViewTextBoxColumn7.MinWidth = 1;
+            gridViewTextBoxColumn7.Name = "Client";
+            gridViewTextBoxColumn7.Width = 200;
+            gridViewTextBoxColumn8.DataType = typeof(System.DateTime);
+            gridViewTextBoxColumn8.FieldName = "OrderDate";
+            gridViewTextBoxColumn8.FormatString = "{0: dd/M/yyyy}";
+            gridViewTextBoxColumn8.HeaderText = "Fecha Pedido";
+            gridViewTextBoxColumn8.MinWidth = 1;
+            gridViewTextBoxColumn8.Name = "OrderDate";
+            gridViewTextBoxColumn8.Width = 120;
+            gridViewTextBoxColumn9.FieldName = "Dozens";
+            gridViewTextBoxColumn9.HeaderText = "Docenas";
+            gridViewTextBoxColumn9.MinWidth = 1;
+            gridViewTextBoxColumn9.Name = "Dozens";
+            gridViewTextBoxColumn9.Width = 80;
+            gridViewTextBoxColumn10.FieldName = "PreparedDate";
+            gridViewTextBoxColumn10.FormatString = "{0: dd/M/yyyy}";
+            gridViewTextBoxColumn10.HeaderText = "Fecha Armado";
+            gridViewTextBoxColumn10.MinWidth = 1;
+            gridViewTextBoxColumn10.Name = "PreparedDate";
+            gridViewTextBoxColumn10.Width = 120;
+            this.dgvPreparedOrders.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
+            gridViewTextBoxColumn6,
+            gridViewTextBoxColumn7,
+            gridViewTextBoxColumn8,
+            gridViewTextBoxColumn9,
+            gridViewTextBoxColumn10});
+            this.dgvPreparedOrders.MasterTemplate.ViewDefinition = tableViewDefinition2;
+            this.dgvPreparedOrders.Name = "dgvPreparedOrders";
+            this.dgvPreparedOrders.ReadOnly = true;
+            this.dgvPreparedOrders.Size = new System.Drawing.Size(652, 195);
+            this.dgvPreparedOrders.TabIndex = 2;
+            this.dgvPreparedOrders.Text = "radGridView1";
+            this.dgvPreparedOrders.ThemeName = "TelerikMetroBlue";
+            // 
+            // panel6
+            // 
+            this.panel6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel6.Controls.Add(this.tableLayoutPanel4);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel6.Location = new System.Drawing.Point(710, 10);
+            this.panel6.Margin = new System.Windows.Forms.Padding(10);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(628, 583);
+            this.panel6.TabIndex = 3;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.dgvPendingOrders, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.label6, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 2;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.01549F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 88.98451F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(626, 581);
+            this.tableLayoutPanel4.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(140, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(345, 32);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "PEDIDOS PENDIENTES";
             // 
             // dgvPendingOrders
             // 
             this.dgvPendingOrders.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvPendingOrders.Location = new System.Drawing.Point(10, 70);
+            this.dgvPendingOrders.Location = new System.Drawing.Point(10, 73);
             this.dgvPendingOrders.Margin = new System.Windows.Forms.Padding(10);
             // 
             // 
@@ -271,6 +374,7 @@
             gridViewTextBoxColumn2.Width = 200;
             gridViewTextBoxColumn3.DataType = typeof(System.DateTime);
             gridViewTextBoxColumn3.FieldName = "OrderDate";
+            gridViewTextBoxColumn3.FormatString = "{0: dd/M/yyyy}";
             gridViewTextBoxColumn3.HeaderText = "Fecha Pedido";
             gridViewTextBoxColumn3.MinWidth = 120;
             gridViewTextBoxColumn3.Name = "OrderDate";
@@ -280,23 +384,29 @@
             gridViewTextBoxColumn4.MinWidth = 120;
             gridViewTextBoxColumn4.Name = "Dozens";
             gridViewTextBoxColumn4.Width = 120;
+            gridViewTextBoxColumn5.FieldName = "Status";
+            gridViewTextBoxColumn5.HeaderText = "Estado";
+            gridViewTextBoxColumn5.Name = "Status";
+            gridViewTextBoxColumn5.Width = 150;
             this.dgvPendingOrders.MasterTemplate.Columns.AddRange(new Telerik.WinControls.UI.GridViewDataColumn[] {
             gridViewTextBoxColumn1,
             gridViewTextBoxColumn2,
             gridViewTextBoxColumn3,
-            gridViewTextBoxColumn4});
+            gridViewTextBoxColumn4,
+            gridViewTextBoxColumn5});
             this.dgvPendingOrders.MasterTemplate.ViewDefinition = tableViewDefinition1;
             this.dgvPendingOrders.Name = "dgvPendingOrders";
             this.dgvPendingOrders.ReadOnly = true;
-            this.dgvPendingOrders.Size = new System.Drawing.Size(530, 179);
-            this.dgvPendingOrders.TabIndex = 2;
+            this.dgvPendingOrders.Size = new System.Drawing.Size(606, 498);
+            this.dgvPendingOrders.TabIndex = 7;
             this.dgvPendingOrders.Text = "radGridView1";
+            this.dgvPendingOrders.ThemeName = "TelerikMetroBlue";
             // 
             // DepositDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1095, 603);
+            this.ClientSize = new System.Drawing.Size(1348, 603);
             this.ControlBox = false;
             this.Controls.Add(this.tableLayoutPanel1);
             this.MaximizeBox = false;
@@ -321,6 +431,11 @@
             this.panel5.ResumeLayout(false);
             this.tableLayoutPanel3.ResumeLayout(false);
             this.tableLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreparedOrders.MasterTemplate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPreparedOrders)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders.MasterTemplate)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders)).EndInit();
             this.ResumeLayout(false);
@@ -345,6 +460,10 @@
         private System.Windows.Forms.Panel panel5;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
         private System.Windows.Forms.Label label5;
+        private Telerik.WinControls.UI.RadGridView dgvPreparedOrders;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
         private Telerik.WinControls.UI.RadGridView dgvPendingOrders;
+        private System.Windows.Forms.Label label6;
     }
 }
