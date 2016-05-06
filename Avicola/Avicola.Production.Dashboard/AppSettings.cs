8ba3ab39a1e5a9f26c0af69bf369ab52dc7b7220
nicolasdogi <nicolasdogi@gmail.com>
2016-05-06 17:02:12 -0300

    cosas del commit anterior

---
+++ Avicola/Avicola.Production.Dashboard/AppSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avicola.Production.Dashboard
+{
+    public class AppSettings
+    {
+        public static string SyncTables
+        {
+            get { return ConfigurationManager.AppSettings["syncTables"]; }
+        }
+
+        public static string ScopeName
+        {
+            get { return ConfigurationManager.AppSettings["ScopeName"]; }
+        }
+
+        public static int SyncPeriod
+        {
+            get { return Convert.ToInt32(ConfigurationManager.AppSettings["SyncPeriod"]); }
+        }
+    }
+}
---
+++ Avicola/Avicola.Production.Dashboard/App_Start/IoCConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avicola.Common.Win.IoC;
+using Ninject;
+
+namespace Avicola.Production.Dashboard.App_Start
+{
+    public class IoCConfig
+    {
+        public static void Configure(StandardKernel kernel)
+        {
+            IoCConfigBase.Configure(kernel);
+        }
+    }
+}
